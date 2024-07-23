682c02fd7ff0b38e26b31ec0a15f2fbf4ea3a89e
John McCarthy <john@flowethic.com>
2024-07-23 23:48:25 +0700

    adjust height message

--- public/iframeMessage.js
+++ public/iframeMessage.js
@@ -1,16 +1,26 @@
-// Function to send the iframe height to parent
+// // Function to send the iframe height to parent
+// function sendHeight() {
+// 	var height = document.body.scrollHeight;
+// 	window.parent.postMessage({ height: height }, 'https://soltukt.test');
+// }
+
+// // Adjust height on load
+// window.onload = sendHeight;
+
+// // Adjust height on content changes
+// var observer = new MutationObserver(sendHeight);
+// observer.observe(document.body, {
+// 	attributes: true,
+// 	childList: true,
+// 	subtree: true
+// });
+
 function sendHeight() {
-	var height = document.body.scrollHeight;
-	window.parent.postMessage({ height: height }, 'https://soltukt.test');
+    var height = document.body.scrollHeight;
+    window.parent.postMessage({ height: height }, '*');
 }
 
-// Adjust height on load
 window.onload = sendHeight;
 
-// Adjust height on content changes
-var observer = new MutationObserver(sendHeight);
-observer.observe(document.body, {
-	attributes: true,
-	childList: true,
-	subtree: true
-});
+var observer = new ResizeObserver(sendHeight);
+observer.observe(document.body);
