ad9abe494f28a1d158e424c80512d4bc2a936300
John McCarthy <john@flowethic.com>
2024-08-30 10:23:20 +0700

    update email template

--- emails/clash.ts
+++ emails/clash.ts
@@ -14,19 +14,17 @@ export function getClashEmailContent(params: EmailData["params"]): string {
                   ? `<p><strong>Member Level: </strong>${params.MemberLevel}</p>`
                   : ""
           }
-		  ${renderShowCategory(params)}
-          ${
-              params?.clashEmails
-                  ? `<p><strong>Press Contact Emails: </strong>${params.clashEmails}</p>`
-                  : ""
-          }
 		  <p>You are receiving this email because you also have a First Night Diary booking on that date.</p>
-		  <p>You can see the full diary here [link to diary] and you can edit or delete your entry on the diary itself.</p>
-		  <p>If you have any questions, please contact Jen <a href="mailto:Jen.dicksonpurdy@soltukt.co.uk?subject=First%20Night%20Diary%20query">Jen.dicksonpurdy@soltukt.co.uk</a></p>
+		  <p>Full clash details are attached as a CSV file, and press contacts with clashes on that date are listed below.</p>
+            ${
+                params?.clashEmails
+                    ? `<p><strong>Press Contact Emails: </strong>${params.clashEmails}</p>`
+                    : ""
+            }
+            <p>You can see the full diary here <a href="https://solt.co.uk/clash-diary">https://solt.co.uk/clash-diary</a> and you can edit or delete your entry on the diary itself.</p>
+		    <p>If you have any questions, please contact Jen <a href="mailto:Jen.dicksonpurdy@soltukt.co.uk?subject=First%20Night%20Diary%20query">Jen.dicksonpurdy@soltukt.co.uk</a></p>
 		  <br/>
 		  <p>Best wishes,</p>
-		  <p>Jen Dickson-Purdy</p>
-		  <p>Marketing Coordinator</p>
 		  <p>SOLT & UK Theatre</p>
 		</body>
 	  </html>
